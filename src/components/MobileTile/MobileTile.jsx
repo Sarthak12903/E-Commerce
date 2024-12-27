889bcacdb80bf05478aa6a60a3b82065851334db
AlinaPathan <alinapathan2004@gmail.com>
2024-12-27 22:48:21 +0530

    design updated

--- src/components/MobileTile/MobileTile.jsx
+++ src/components/MobileTile/MobileTile.jsx
@@ -2,49 +2,55 @@ import React from "react";
 import { LiaRupeeSignSolid } from "react-icons/lia";
 import { FaIndianRupeeSign } from "react-icons/fa6";
 
-const MobileTile = () => {
+const MobileTile = ({mobileName, moPrice, mobileImg,rating,moRating,moReviews, moSize ,moRam, moCamera,moBattery,moProcessor,moWarrenty}) => {
   return (
-    <div className="flex p-4 w-auto ">
-      <div className="p-4">
-        <img src="public\CategoriesImg\Mobiles\samsungs23.webp" alt="" />
-      </div>
-      <div className="flex flex-col justfy-between gap-2  p-8">
-        <h1 className=" p-1 text-md">Samsung S23 Ultra (256GB)</h1>
-        <p className="text-sm text-grey-100 flex items-center justify-start gap-4 ">
-          <img
-            className="h-[1.2rem] w-[2.4rem] rounded-sm"
-            src="public\CategoriesImg\Mobiles\rating.png"
-          />
-          <p>65179 Ratings & 4967 Reviews</p>
+    <div className="flex p-4 w-auto items-start gap-2 pr-20">
+      <div className="p-6">
+        <img className="w-[12rem] h-[14rem]" src={mobileImg || '\CategoriesImg\Mobiles\samsungs23.webp'} alt="" />
+      </div> 
+      <div className="flex flex-col justfy-between gap-1 p-4 ">
+        <div>
+        <h1 className=" pt-1 px-1 text-[1.2rem] font-semibold">{mobileName|| 'Name not retrived'}</h1>
+        <p className="text-sm text-grey-100 flex items-center justify-start gap-4 p-2">
+          <div
+            className="h-[1.2rem] w-[2.4rem] text-white flex justify-center items-center rounded-sm bg-green-600">
+          {rating ||"0.0"}
+         </div>
+          <p className="text-gray-500 font-semibold">{moRating ||"Loading"} Rating & {moReviews || "Loading"}  Reviews</p>
         </p>
+        </div>
 
-        <ul className="flex flex-col gap-2 justify-around items-start list-disc list-inside">
-          <li className="text-sm color-grey-200">8 GB RAM | 128 GB ROM</li>
-          <li className="text-sm color-grey-200">15.49 cm (6.1 inch) Full HD+ Display</li>
-          <li className="text-sm color-grey-200">15 MP |12 MP |10 MP (12 MP FRont Camera)</li>
-          <li className="text-sm color-grey-200">3900 mAh Lithium Ion Battery</li>
-          <li className="text-sm color-grey-200">Qualcomm Snapdragon 8 Gen 2 Processor</li>
-          <li className="text-sm color-grey-200">1 year msnufacturer warrenty for device and 6 months manufacturer
-          warrenty for in box accesories</li>
+        <ul className="flex flex-col p-2 gap-1 text-[0.8rem] text-gray-800 justify-around items-start list-disc list-inside">
+          <li>{moRam || "8 GB RAM | 128 GB ROM"}</li>
+          <li>{moSize||"15.49 cm (6.1 inch) Full HD+ Display"}</li>
+          <li>{moCamera ||"15 MP |12 MP |10 MP (12 MP FRont Camera)"}</li>
+          <li>{moBattery || "3900 mAh Lithium Ion Battery"}</li>
+          <li>{moProcessor ||"Qualcomm Snapdragon 8 Gen 2 Processor"}</li>
+          <li>{moWarrenty ||"1 year msnufacturer warrenty for device and 6 months manufacturer  warrenty for in box accesories"}</li>
         
         </ul>
       </div>
-      <div className="p-8">
-        <p className="font-semibold flex items-center text-[2rem]">
+      <div  className="p-4">
+        <p className="font-semibold flex items-center text-[1.5rem]">
         
           <FaIndianRupeeSign />
-          <h1>49000</h1>
+          <h1>{moPrice ||49000}</h1>
         </p>
-        <p className="flex items-center">
+        <p className="flex items-center text-sm">
           <LiaRupeeSignSolid />
-          <p className="line-through">89000</p>
-          <p className="mx-2"> upto 45% Off</p>
+          <p className="line-through text-gray-500">89000</p>
+          <p className="mx-2 text-orange-600 font-semibold"> upto 45% Off</p>
         </p>
-        <p>Free Delivery</p>
-        <p className="flex items-center">
+        <p className="text-sm">Free Delivery</p>
+        <p className="flex items-end text-[0.7rem] font-semibold text-orange-600">
         
-          <p>Upto</p> <LiaRupeeSignSolid /> <p>750 Off on Airtel Offer</p>
+          <p>Upto  </p> <LiaRupeeSignSolid className="h-[1rem] w-[1rem] "/> <p>750 Off on Airtel Offer</p>
         </p>
+        <p className="flex items-center text-[0.8rem] ">
+        
+          <p>Upto  </p> <LiaRupeeSignSolid className="h-[0.8rem] w-[0.8rem]"/> <p>3400 Off on Exchange Offer</p>
+        </p>
+        
       </div>
     </div>
   );
--- src/pages/Mobile/Mobile.jsx
+++ src/pages/Mobile/Mobile.jsx
@@ -1,21 +1,116 @@
-import React from 'react'
-import MobileTile from '../../components/MobileTile/MobileTile'
-import Filters from '../../components/Filters/Filters'
+import React from "react";
+import MobileTile from "../../components/MobileTile/MobileTile";
+import Filters from "../../components/Filters/Filters";
+// import { SlArrowRight } from "react-icons/sl";
+import { RiArrowRightSLine } from "react-icons/ri";
+
 const Mobile = () => {
   return (
-    <div className='flex m-4 justify-between gap-2'>
-       <Filters/>
-       <div px-6>
-        <MobileTile/>
-        <MobileTile/>
-        <MobileTile/>
-        <MobileTile/>
-        <MobileTile/>
-        <MobileTile/>
-        <MobileTile/>
-       </div>
-    </div>
-  )
-}
+    <div className="flex m-4 justify-between gap-2">
+      <Filters />
+      <div px-6>
+        <div className="px-2">
+          <div className="flex text-[0.8rem] text-gray-600 items-center gap-[0.4rem]">
+            <h1 className="hover:text-orange-500">Home</h1>
+            <RiArrowRightSLine />
 
-export default Mobile
+            <p className="hover:text-orange-500"> Mobiles</p>
+          </div>
+          <p className="text-lg font-semibold ">
+            Showing Popular Smartphones at Great Prices
+          </p>
+          <ul className="flex justify-start gap-4  text-gray-500 text-sm py-2">
+            <li className="font-bold">Sort By</li>
+            <li className="hover:text-orange-500">Relevence</li>
+            <li className="hover:text-orange-500">Popularity</li>
+            <li className="hover:text-orange-500">Prices (Low to High)</li>
+            <li className="hover:text-orange-500">Prices (High to Low)</li>
+            <li className="hover:text-orange-500">Latest First</li>
+          
+          </ul>
+        </div>
+        <MobileTile mobileImg="\CategoriesImg\Mobiles\samsungs23.webp" />
+        <MobileTile
+          mobileName="IPhone 16 (Ultramarine) 128GB"
+          moPrice=""
+          mobileImg="\CategoriesImg\Mobiles\googlepixal8a.webp"
+          moRating=""
+          moSize=""
+          moRam=""
+          moCamera=""
+          moBattery=""
+          moProcessor=""
+          moWarrenty=""
+        />
+        <MobileTile
+          mobileName="IPhone 16 (Ultramarine) 128GB"
+          moPrice=""
+          mobileImg="\CategoriesImg\Mobiles\oneplusnordce.webp"
+          ratingImg=""
+          moRating=""
+          moSize=""
+          moRam=""
+          moCamera=""
+          moBattery=""
+          moProcessor=""
+          moWarrenty=""
+        />
+        <MobileTile
+          mobileName="IPhone 16 (Ultramarine) 128GB"
+          moPrice=""
+          mobileImg="\CategoriesImg\Mobiles\oppok12.webp"
+          ratingImg=""
+          moRating=""
+          moSize=""
+          moRam=""
+          moCamera=""
+          moBattery=""
+          moProcessor=""
+          moWarrenty=""
+        />
+        <MobileTile
+          mobileName="IPhone 16 (Ultramarine) 128GB"
+          moPrice=""
+          mobileImg="\CategoriesImg\Mobiles\iphone.webp"
+          ratingImg=""
+          moRating=""
+          moSize=""
+          moRam=""
+          moCamera=""
+          moBattery=""
+          moProcessor=""
+          moWarrenty=""
+        />
+        <MobileTile
+          mobileName="IPhone 16 (Ultramarine) 128GB"
+          moPrice=""
+          mobileImg="\CategoriesImg\Mobiles\vivot3.webp"
+          ratingImg=""
+          moRating=""
+          moSize=""
+          moRam=""
+          moCamera=""
+          moBattery=""
+          moProcessor=""
+          moWarrenty=""
+        />
+
+        <MobileTile
+          mobileName="IPhone 16 (Ultramarine) 128GB"
+          moPrice=""
+          mobileImg="\CategoriesImg\Mobiles\realme12.webp"
+          ratingImg=""
+          moRating=""
+          moSize=""
+          moRam=""
+          moCamera=""
+          moBattery=""
+          moProcessor=""
+          moWarrenty=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Mobile;
